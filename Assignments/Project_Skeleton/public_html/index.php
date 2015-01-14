a995c7179aeed03c3a6710dcda00b84e436059ac
unknown <nly10130@CLK310-07.vtc.vsc.edu>
2015-01-13 19:49:38 -0500

    Done

---
+++ Assignments/Project_Skeleton/public_html/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<title>Hello World </title>
+</head>
+
+<body bgcolor="Black" text="blue">
+
+<h1> Hello World</h1>
+
+<?php
+	echo "Hello World!";
+?>
+
+</body>
+
+</html>
